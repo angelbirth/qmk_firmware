1d0338aad928e44941ff741e07c6f93bdd4c5730
Engelbert <ricky.ason@gmail.com>
2020-03-11 08:54:28 +0700

    initial commit

---
+++ keyboards/egb980/egb980.h
@@ -0,0 +1,47 @@
+/* Copyright 2020 Engelbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "quantum.h"
+
+/* This is a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+
+#define KNO KC_NO
+
+#define LAYOUT_all( \
+      k000, k001, k002, k003, k004, k005, k006, k007, k008, k009, k00a, k00b, k00c, k00d, k00e, k00f, k010, k011, k012, \
+      k100, k101, k102, k103, k104, k105, k106, k107, k108, k109, k10a, k10b, k10c, k10d, k10e, k10f, k110, k111, k112, \
+      k200, k201, k202, k203, k204, k205, k206, k207, k208, k209, k20a, k20b, k20c, k20d,       k20f, k210, k211, k212, \
+      k300, k301, k302, k303, k304, k305, k306, k307, k308, k309, k30a, k30b,       k30d,       k30f, k310, k311, k312, \
+      k400, k401, k402, k403, k404, k405, k406, k407, k408, k409, k40a,       k40c, k40d,       k40f, k410, k411, k412, \
+      k500, k501, k502, k503, k504,       k506, k507,       k509, k50a,       k50c, k50d,       k50f, k510, k511, k512  \
+) \
+{ \
+    { k000, k001, k002, k003, k004, k005, k006, k007, k008, k009, k00a, k00b, k00c, k00d, k00e, k00f, k010, k011, k012 }, \
+    { k100, k101, k102, k103, k104, k105, k106, k107, k108, k109, k10a, k10b, k10c, k10d, k10e, k10f, k110, k111, k112 }, \
+    { k200, k201, k202, k203, k204, k205, k206, k207, k208, k209, k20a, k20b, k20c, k20d, KNO,  k20f, k210, k211, k212 },\
+    { k300, k301, k302, k303, k304, k305, k306, k307, k308, k309, k30a, k30b, KNO,  k30d, KNO,  k30f, k310, k311, k312 }, \
+    { k400, k401, k402, k403, k404, k405, k406, k407, k408, k409, k40a, KNO,  k40c, k40d, KNO,  k40f, k410, k411, k412 }, \
+    { k500, k501, k502, k503, k504, KNO,  k506, k507, KNO,  k509, k50a, KNO,  k50c, k50d, KNO,  k50f, k510, k511, k512 }  \
+}
---
+++ keyboards/egb980/rules.mk
@@ -0,0 +1,53 @@
+# MCU name
+MCU = STM32F103
+
+# GENERIC STM32F103C8T6 board - stm32duino bootloader
+OPT_DEFS = -DCORTEX_VTOR_INIT=0x2000
+MCU_LDSCRIPT = STM32F103x8_stm32duino_bootloader
+BOARD = STM32_F103_STM32DUINO
+
+DFU_ARGS = -d 1eaf:0003 -a2 -R
+DFU_SUFFIX_ARGS = -v 1eaf -p 0003
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+#BOOTLOADER = atmel-dfu
+
+# If you don't know the bootloader type, then you can specify the
+# Boot Section Size in *bytes* by uncommenting out the OPT_DEFS line
+# Otherwise, delete this section
+#   Teensy halfKay      512
+#   Teensy++ halfKay    1024
+#   Atmel DFU loader    4096
+#   LUFA bootloader     4096
+#   USBaspLoader        2048
+# OPT_DEFS += -DBOOTLOADER_SIZE=4096
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output on port C6
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
